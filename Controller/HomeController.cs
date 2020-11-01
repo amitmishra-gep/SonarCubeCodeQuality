6ebf6a79106c0a71cb380e2a5baf0e2f04a74802
Amar Barge <Amar.Barge@gep.com>
2020-11-01 13:03:48 +0530

    New Smells

--- Controller/HomeController.cs
+++ Controller/HomeController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+using System.Globalization;
+using System.Diagnostics;
 
 namespace SonarCubeCodeQuality
 {
