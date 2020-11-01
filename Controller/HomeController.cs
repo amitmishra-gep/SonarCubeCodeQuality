db89ac5f9971dd1d2ab0b10d8a25b54af16f8f3b
Amar Barge <Amar.Barge@gep.com>
2020-11-01 12:45:01 +0530

    New Controller Introduced

---
+++ Controller/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SonarCubeCodeQuality
+{
+    public class HomeController : Controller
+    {
+        //New Controller Introduced
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
